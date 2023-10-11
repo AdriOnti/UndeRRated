76bd6dd78b47bbd32d393ede67587292e0fb812c
LilBrainer <josepuy71@gmail.com>
2023-10-11 17:45:23 +0200

    Movement to left and right and slide

--- UndeRRated/Assets/Olav/Scripts/RatController.cs
+++ UndeRRated/Assets/Olav/Scripts/RatController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class RatController : MonoBehaviour
 {
@@ -11,6 +14,8 @@ public class RatController : MonoBehaviour
     private int desiredPath = 1;    // 0: Left Path, 1: Middle Path, 2: Right Path
     public float pathDistance = 4;
 
+    public float forwardSpeed;
+
     public float jumpForce;
     public float Gravity = -20;
 
@@ -18,12 +23,20 @@ public class RatController : MonoBehaviour
     private bool isSliding;
 
     // METHODS
-    void Start() { controller = GetComponent<CharacterController>(); }
+    void Start() 
+    { 
+        controller = GetComponent<CharacterController>();
+    }
 
 
     // TO MOVE
     void Update()
     {
+
+        if (transform.position.y < 4.0f)
+        {
+            transform.position = new Vector3(0, 5.13f, 0);
+        }
         // if the character is in the ground, you can jump
         if (controller.isGrounded)
         {
@@ -33,6 +46,8 @@ public class RatController : MonoBehaviour
         else { direction.y += Gravity * Time.deltaTime; }
 
         if (Input.GetKeyUp(KeyCode.DownArrow) && !isSliding) { StartCoroutine(Slide()); }
+
+        direction.z = forwardSpeed;
 
         // Move to the right path
         if (Input.GetKeyUp(KeyCode.RightArrow))
@@ -48,12 +63,12 @@ public class RatController : MonoBehaviour
             if (desiredPath <= -1) desiredPath = 0;
         }
 
-        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        Vector3 targetPosition = transform.position.y * transform.up;
 
-        if (desiredPath == 0) { targetPosition += Vector3.left * pathDistance; }
-        else if (desiredPath == 2) { targetPosition = Vector3.right * pathDistance; }
+        if(desiredPath == 0) { targetPosition += Vector3.left * pathDistance; }
+        else if(desiredPath == 2) { targetPosition += Vector3.right * pathDistance; }
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, 80 * Time.deltaTime);
+        transform.position = targetPosition;
     }
 
     private void FixedUpdate() { controller.Move(direction * Time.fixedDeltaTime); }
@@ -66,16 +81,28 @@ public class RatController : MonoBehaviour
     {
         // Slide turn on
         isSliding = true;
-        //animator.SetBool("isSliding", true);
-        controller.center = new Vector3(0, -0.5f, 0);
-        controller.height = 1;
 
-        yield return new WaitForSeconds(1.3f);
+        float slideDuration = 0.3f;
+        float elapsedTime = 0;
+        Vector3 startPos = transform.position;
+
+        while (elapsedTime < slideDuration)
+        {
+            float slideAmount = Mathf.Lerp(0, -0.5f, elapsedTime / slideDuration);
+            transform.position = new Vector3(transform.position.x, startPos.y + slideAmount, transform.position.z);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
 
         // Slide turn off
-        controller.center = new Vector3(0, 0, 0);
-        controller.height = 2;
-        //animator.SetBool("isSliding", false);
         isSliding = false;
+        elapsedTime = 0;
+        while(elapsedTime < slideDuration)
+        {
+            float slideAmount = Mathf.Lerp(0, 0.15f, elapsedTime/ slideDuration);
+            transform.position = new Vector3(transform.position.x, startPos.y + slideAmount, transform.position.z);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
