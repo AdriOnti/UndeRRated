c1980d620cc7529b7b0294913c66ffaffb4d6202
AdriOnti <adrian.ontiveros.7e4@itb.cat>
2023-10-27 15:35:05 +0200

    reorder unity good

--- UndeRRated/Assets/Scripts/RatController.cs
+++ UndeRRated/Assets/Scripts/RatController.cs
@@ -1,59 +1,79 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class RatController : MonoBehaviour
 {
+    // PRIVATE PARAMETERS
     private CharacterController controller;
     private Vector3 direction;
     private int desiredPath = 1;
+    private Animator animator;
+
+    // PUBLIC PARAMETERS
     public float jumpForce;
     public float pathDistance = 10;
     public float Gravity;
 
-
+    // METHOD START
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
+    // METHOD UPDATE
     void Update()
     {
         controller.Move(direction * Time.deltaTime);
 
-        if (controller.isGrounded && Input.GetKeyDown(KeyCode.UpArrow)) Jump();
-        else direction.y += Gravity*2 * Time.deltaTime;
+        // JUMP
+        if(controller.isGrounded && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))) Jump(); 
+        else direction.y += Gravity * 2 * Time.deltaTime;
 
-        if (!controller.isGrounded && Input.GetKeyDown(KeyCode.DownArrow)) direction.y -= jumpForce;
+        // FORCE TO GO TO THE GROUND IF IS JUMPING
+        if(!controller.isGrounded && (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))) direction.y -= jumpForce;
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        // CALCULATE THE RIGHT PATH
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
             desiredPath++;
             if (desiredPath >= 3) desiredPath = 2;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        // CALCULATE THE LEFT PATH
+        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
             desiredPath--;
-            if (desiredPath <= -1) desiredPath = 0;
+            if(desiredPath <= -1) desiredPath = 0;
         }
 
+        // MOVE TO THE PATH
         GoToPath();
 
+        // SLIDE
+        if (controller.isGrounded && (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S)))
+        {
+            animator.Play("slide");
+            StartCoroutine(StopAnimation());
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Shot();
+        }
+        
     }
 
+    // STOP TIME IF PLAYER IMPACT WITH AN OBSTACLE
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("ObstacleGeneric") || other.gameObject.CompareTag("ObstacleHN") || other.gameObject.CompareTag("ObstacleHT")) Time.timeScale = 0;
+        if (other.gameObject.CompareTag("ObstacleGeneric")) Time.timeScale = 0;
     }
 
-    private void Jump()
-    {
-        direction.y = jumpForce;
-    }
+    // JUMP FUNCTION
+    private void Jump() { direction.y = jumpForce; }
 
+    // MOVEMENT TO THE DESIRED PATH FUNCTION
     private void GoToPath()
     {
         Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
@@ -62,5 +82,20 @@ public class RatController : MonoBehaviour
         if (desiredPath == 2) targetPosition += Vector3.right * pathDistance;
 
         transform.position = Vector3.Lerp(transform.position, targetPosition, 0.25f);
+    }
+
+    // STOP ANIMATION
+    private IEnumerator StopAnimation()
+    {
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        animator.enabled = false;
+        yield return new WaitForSeconds(0.01f);
+        animator.enabled = true;
+    }
+
+    // SHOT
+    private void Shot()
+    {
+        Debug.Log("He disparado");
     }
 }
