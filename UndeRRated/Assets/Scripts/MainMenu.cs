f134ac92a98567a1dda07b4dd3629fdc839dc496
OlavMartos <olav.martos.7e4@itb.cat>
2023-11-27 15:00:53 +0100

    Falta animaciones simultaneas

--- UndeRRated/Assets/Scripts/MainMenu.cs
+++ UndeRRated/Assets/Scripts/MainMenu.cs
@@ -5,13 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : Menu
 {
+    private Animation animation;
+    private Animator animator;
+
     /// <summary>
     /// Cuando el usuario le de al boton de comenzar partida, se cargara la escena 
     /// </summary>
     public void StartGame()
     {
         Time.timeScale = 1.0f;
-        SceneManager.LoadScene("UndeRRated");
+        CameraInStart.animIsStart = true;
+        animator = rat.GetComponent<Animator>();
+        animator.enabled = true;
+
+        animator.Play("startGame");
+        
+        // Se han de ejecutar la animacion de correr y la de StartGame al mismo tiempo
+
+        //animation.CrossFade("Armature_ArmatureAction");
+        
+        //SceneManager.LoadScene("UndeRRated");
     }
 
     public void RatShop()
