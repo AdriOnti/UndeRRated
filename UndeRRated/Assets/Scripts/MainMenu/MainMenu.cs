ad3c81a0b34a64ec51aafd88890b9204ba7195fe
OlavMartos <olav.martos.7e4@itb.cat>
2024-01-16 16:33:54 +0100

    Crown FBX + MainMenu Optimizacion

--- UndeRRated/Assets/Scripts/MainMenu/MainMenu.cs
+++ UndeRRated/Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,21 +1,44 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : Menu
 {
-    //private Animation animation;
     private Animator animatorParent;
-    public GameObject mainCanvas;
-    public GameObject howToPlayCanvas;
-    public GameObject shopCanvas;
-    public GameObject achievementCanvas;
+    private GameObject mainCanvas;
+    private GameObject howToPlayCanvas;
+    private GameObject shopCanvas;
+    private GameObject achievementCanvas;
+
+    private void CanvasAssigment()
+    {
+        foreach (GameObject canvas in GetUI())
+        {
+            if (canvas.gameObject.name == "MainCanvas") mainCanvas = canvas;
+            if (canvas.gameObject.name == "Controles") howToPlayCanvas = canvas;
+            if (canvas.gameObject.name == "Shop") shopCanvas = canvas;
+            if (canvas.gameObject.name == "Achievement") achievementCanvas = canvas;
+        }
+    }
+
+    private GameObject[] GetUI()
+    {
+        GameObject father = GameObject.Find("CanvasFather");
+        GameObject[] ui = new GameObject[father.transform.childCount - 1];
+
+        for (int i = 0; i < ui.Length; i++)
+        {
+            if (father.transform.GetChild(i).gameObject.name != "Fade") ui[i] = father.transform.GetChild(i).gameObject;
+        }
+
+        return ui;
+    }
 
     private void OnEnable()
     {
         Time.timeScale = 1.0f;
         try
         {
+            CanvasAssigment();
             mainCanvas.GetComponent<Canvas>().enabled = true;
             howToPlayCanvas.GetComponent<Canvas>().enabled = false;
             shopCanvas.GetComponent<Canvas>().enabled = false;
@@ -29,30 +52,77 @@ public class MainMenu : Menu
     /// </summary>
     public void StartGame()
     {
-
         CameraInStart.Instance.animIsStart = true;
         animatorParent = rat.GetComponent<Animator>();
         mainCanvas.GetComponent<Canvas>().enabled = false;
         animatorParent.SetBool("start", true);
     }
 
-    public void RatShop() { StartCoroutine(ShopIn()); }
+    /// <summary>
+    /// Funcion que llaman los botones del MainMenu
+    /// </summary>
+    /// <param name="sewer">0: MainSewer, 1: HowToPlaySewer, 2: ShopSewer, 3: AchievementSewer</param>
+    public void SewerIn(int sewer)
+    {
+        switch(sewer)
+        {
+            case 0:
+                StartCoroutine(MainIn());
+                break;
+            case 1:
+            case 2:
+            case 3:
+                StartCoroutine(OtherSewer(DecideSewer(sewer)));
+                break;
+        }
+    }
 
-    public void ControlSewer() { StartCoroutine(CntrlIn()); }
+    /// <summary>
+    /// Segun el numero de sewer, se devuelve una string utilizada por el CameraInStart;
+    /// </summary>
+    /// <param name="sewer"></param>
+    /// <returns></returns>
+    private string DecideSewer(int sewer)
+    {
+        switch (sewer)
+        {
+            case 1:
+                return "HowToPlayIn";
+            case 2:
+                return "RatShopIn";
+            case 3:
+                return "AchievementIn";
+        }
+        return null;
+    }
 
-    public void MainSewer() { StartCoroutine(MainIn()); }
-
-    public void AchievementSewer() { StartCoroutine(AchievementIn()); }
-
-    IEnumerator CntrlIn()
+    /// <summary>
+    /// Corrutina que permite el cambio de sewer
+    /// </summary>
+    /// <param name="sewer">Nombre del bool que se quiere</param>
+    IEnumerator OtherSewer(string sewer)
     {
         DisableMainCanvas();
         yield return new WaitForSeconds(0.5f);
-        CameraInStart.Instance.ModifyBools("HowToPlayIn");
+        CameraInStart.Instance.ModifyBools(sewer);
 
         yield return new WaitForSeconds(0.5f);
-        howToPlayCanvas.GetComponent<Canvas>().enabled = true;
+        DecideCanvas(sewer).GetComponent<Canvas>().enabled = true;
         FadeController.instance.FadeIn();
+    }
+
+    private GameObject DecideCanvas(string sewer)
+    {
+        switch (sewer)
+        {
+            case "HowToPlayIn":
+                return howToPlayCanvas;
+            case "RatShopIn":
+                return shopCanvas;
+            case "AchievementIn":
+                return achievementCanvas;
+        }
+        return null;
     }
 
     IEnumerator MainIn()
@@ -67,28 +137,6 @@ public class MainMenu : Menu
 
         yield return new WaitForSeconds(0.5f);
         mainCanvas.GetComponent<Canvas>().enabled = true;
-        FadeController.instance.FadeIn();
-    }
-
-    IEnumerator ShopIn()
-    {
-        DisableMainCanvas();
-        yield return new WaitForSeconds(0.5f);
-        CameraInStart.Instance.ModifyBools("RatShopIn");
-
-        yield return new WaitForSeconds(0.5f);
-        shopCanvas.GetComponent<Canvas>().enabled = true;
-        FadeController.instance.FadeIn();
-    }
-
-    IEnumerator AchievementIn()
-    {
-        DisableMainCanvas();
-        yield return new WaitForSeconds(0.5f);
-        CameraInStart.Instance.ModifyBools("AchievementIn");
-
-        yield return new WaitForSeconds(0.5f);
-        achievementCanvas.GetComponent<Canvas>().enabled = true;
         FadeController.instance.FadeIn();
     }
 
